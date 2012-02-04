99757f11bf3dfee6365ef4745f905aacb1c55a75
Michael Wright <mdwrigh2@ncsu.edu>
2012-02-04 16:12:21 -0500

    Removed linux subdirectory

--- linux/install-zsh-environment.rb
+++
@@ -1,8 +0,0 @@
-#! /usr/bin/env ruby
-# this puts all of the files in the current directory into an array of strings
-# and then iterates over them
-
-home = ENV['HOME']
-Dir["#{Dir.pwd}/*"].each do |file|
-  system "ln -nfs #{file} #{home}/.#{file.split('/').last}" unless file.split('/').last.include?('install-zsh')
-end
