85533cbdd9300396fd2d0ea4b8068a9a48da021c
Michael Wright <mdwrigh2@ncsu.edu>
2012-02-19 23:13:33 -0500

    Improved the installer script by adding ignores

--- install-zsh-environment.rb
+++ install-zsh-environment.rb
@@ -2,7 +2,11 @@
 # this puts all of the files in the current directory into an array of strings
 # and then iterates over them
 
+ignore = ["README",
+          "arch_packages",
+          "install-zsh-environment.rb"] # Don't symlink these
 home = ENV['HOME']
 Dir["#{Dir.pwd}/*"].each do |file|
-  system "ln -nfs #{file} #{home}/.#{file.split('/').last}" unless file.split('/').last.include?('install-zsh')
+  filename = file.split('/').last
+  system "ln -nfs #{file} #{home}/.#{filename}" unless ignore.include? filename
 end
