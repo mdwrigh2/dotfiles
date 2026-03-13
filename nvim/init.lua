2b2667a63ba0909d889c3c9f9f26ef75f0228c10
Michael Wright <m@mdw.dev>
2026-03-13 12:04:19 +0000

    Remove vimrc sourcing from neovim

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,16 +1,2 @@
 require("options")
-
--- Set up runtimepath so Pathogen and bundles are available, then source vimrc
-if vim.fn.has('win32') == 1 then
-  vim.opt.runtimepath:prepend('~/vimfiles')
-  vim.opt.runtimepath:append('~/vimfiles/after')
-  vim.opt.packpath = vim.opt.runtimepath:get()
-  vim.cmd('source ~/_vimrc')
-else
-  vim.opt.runtimepath:prepend('~/.vim')
-  vim.opt.runtimepath:append('~/.vim/after')
-  vim.opt.packpath = vim.opt.runtimepath:get()
-  vim.cmd('source ~/.vimrc')
-end
-
 require("keymaps")
