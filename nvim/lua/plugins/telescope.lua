e60dc01208100c061c316dc14dd6a2167656bfe9
Michael Wright <m@mdw.dev>
2026-03-13 17:00:28 +0000

    Add a few more telescope commands and ignore rules

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -8,8 +8,26 @@ return {
     { '<leader>o', '<cmd>Telescope find_files<cr>', desc = 'Find files' },
     { '<leader>m', '<cmd>Telescope buffers<cr>', desc = 'Buffers' },
     { '<leader>/', '<cmd>Telescope live_grep<cr>', desc = 'Live grep' },
+    { '<leader>?', '<cmd>Telescope oldfiles<cr>', desc = 'Recent files' },
+    { '<leader>ds', '<cmd>Telescope lsp_document_symbols<cr>', desc = 'Document symbols' },
+    { '<leader>ws', '<cmd>Telescope lsp_workspace_symbols<cr>', desc = 'Workspace symbols' },
   },
   config = function()
+    require('telescope').setup({
+      defaults = {
+        file_ignore_patterns = {
+          '%.git/',
+          'node_modules/',
+          'out/',
+          '%.class$',
+          '%.o$',
+          '%.so$',
+          '%.a$',
+          'target/',
+          'dist/',
+        },
+      },
+    })
     require('telescope').load_extension('fzf')
   end,
 }
