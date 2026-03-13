0307f23430373168b70bbe62f2cb9d4ec4864d11
Michael Wright <m@mdw.dev>
2026-03-13 12:43:13 +0000

    Add comment plugin
    Map <leader>c to comment / uncomment lines.

---
+++ nvim/lua/plugins/comment.lua
@@ -0,0 +1,8 @@
+return {
+  'numToStr/Comment.nvim',
+  keys = {
+    { '<leader>c', '<Plug>(comment_toggle_linewise_current)', desc = 'Toggle comment' },
+    { '<leader>c', '<Plug>(comment_toggle_linewise_visual)', mode = 'v', desc = 'Toggle comment' },
+  },
+  opts = {},
+}
