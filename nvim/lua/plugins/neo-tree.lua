362d0f0df54ecf25f18fe4db38cb7b6a28141076
Michael Wright <m@mdw.dev>
2026-03-13 14:19:07 +0000

    Add neo-tree plugin

---
+++ nvim/lua/plugins/neo-tree.lua
@@ -0,0 +1,12 @@
+return {
+  'nvim-neo-tree/neo-tree.nvim',
+  dependencies = {
+    'nvim-lua/plenary.nvim',
+    'nvim-tree/nvim-web-devicons',
+    'MunifTanjim/nui.nvim',
+  },
+  keys = {
+    { '<F12>', '<cmd>Neotree toggle<cr>', desc = 'Toggle file explorer' },
+  },
+  opts = {},
+}
