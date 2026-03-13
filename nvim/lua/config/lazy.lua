adff096411b2421ff537b47a455d8dacbad03e9f
Michael Wright <m@mdw.dev>
2026-03-13 12:07:43 +0000

    Add lazy.nvim

---
+++ nvim/lua/config/lazy.lua
@@ -0,0 +1,24 @@
+-- Bootstrap lazy.nvim
+local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  local out = vim.fn.system({
+    'git', 'clone', '--filter=blob:none',
+    'https://github.com/folke/lazy.nvim.git',
+    '--branch=stable',
+    lazypath,
+  })
+  if vim.v.shell_error ~= 0 then
+    vim.api.nvim_echo({
+      { 'Failed to clone lazy.nvim:\n', 'ErrorMsg' },
+      { out, 'WarningMsg' },
+    }, true, {})
+  end
+end
+vim.opt.rtp:prepend(lazypath)
+
+require('lazy').setup({
+  spec = {
+    { import = 'plugins' },
+  },
+  checker = { enabled = true },
+})
