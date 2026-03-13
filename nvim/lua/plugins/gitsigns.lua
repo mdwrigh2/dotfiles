a439a895cb683e454f1c4ed8ab439e9d1504ac24
Michael Wright <m@mdw.dev>
2026-03-13 16:24:21 +0000

    Add some gitsigns keymappings

--- nvim/lua/plugins/gitsigns.lua
+++ nvim/lua/plugins/gitsigns.lua
@@ -1,4 +1,19 @@
 return {
   'lewis6991/gitsigns.nvim',
-  opts = {},
+  opts = {
+    on_attach = function(bufnr)
+      local gs = require('gitsigns')
+      local map = function(keys, func, desc)
+        vim.keymap.set('n', keys, func, { buffer = bufnr, desc = desc })
+      end
+
+      map(']h', gs.next_hunk, 'Next hunk')
+      map('[h', gs.prev_hunk, 'Previous hunk')
+      map('<leader>hs', gs.stage_hunk, 'Stage hunk')
+      map('<leader>hr', gs.reset_hunk, 'Reset hunk')
+      map('<leader>hp', gs.preview_hunk_inline, 'Preview hunk inline')
+      map('<leader>hb', gs.toggle_current_line_blame, 'Toggle line blame')
+      map('<leader>hd', gs.diffthis, 'Diff this file')
+    end,
+  },
 }
