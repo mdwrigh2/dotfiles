7706b17ae5abf0b98bb4c06b49249259a350a1c8
Michael Wright <m@mdw.dev>
2026-03-13 11:43:53 +0000

    Rewrite nvim init in lua

---
+++ nvim/init.lua
@@ -0,0 +1,12 @@
+-- Set up runtimepath so Pathogen and bundles are available, then source vimrc
+if vim.fn.has('win32') == 1 then
+  vim.opt.runtimepath:prepend('~/vimfiles')
+  vim.opt.runtimepath:append('~/vimfiles/after')
+  vim.opt.packpath = vim.opt.runtimepath:get()
+  vim.cmd('source ~/_vimrc')
+else
+  vim.opt.runtimepath:prepend('~/.vim')
+  vim.opt.runtimepath:append('~/.vim/after')
+  vim.opt.packpath = vim.opt.runtimepath:get()
+  vim.cmd('source ~/.vimrc')
+end
